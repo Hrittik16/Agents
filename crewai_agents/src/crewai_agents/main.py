d71dc157493a5bb64e3dcd1ef283dc6deb461fc1
Hrittik16 <hrittikd16@gmail.com>
2025-04-27 13:17:43 +0000

    crewai agents added

---
+++ crewai_agents/src/crewai_agents/main.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from datetime import datetime
+
+from crewai_agents.crew import CrewaiAgents
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    input_array = [
+        {
+            'topic': 'HR Tech Events After May 2025',
+            'current_year': str(datetime.now().year)
+        },
+        {
+            'topic': 'HR Events After May 2025',
+            'current_year': str(datetime.now().year)
+        }, 
+    ]
+    
+    try:
+        CrewaiAgents().crew().kickoff_for_each(inputs=input_array)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+'''
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        CrewaiAgents().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        CrewaiAgents().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "AI LLMs",
+        "current_year": str(datetime.now().year)
+    }
+    try:
+        CrewaiAgents().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while testing the crew: {e}")
+'''
